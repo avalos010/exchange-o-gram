593be27a71244d256980a836108fa4bb6227c1dc
avalos010 <luizavalos40@gmail.com>
2020-02-17 18:11:39 -0600

    only show posts from users that are being followed or your own.

--- post/views.py
+++ post/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponse
 from django.contrib import messages
 from userprofile.models import UserProfile
 from .models import Post
+from django.db.models import Q
 
 # Create your views here.
 
 
 def feed(request):
     if request.user.is_authenticated:
-        posts = Post.objects.order_by('-date')
+        posts = Post.objects.filter(
+            Q(profile__followers=request.user.userprofile.id) | Q(profile_id=request.user.userprofile.id)).order_by('-date')
 
         return render(request, 'posts/feed.html', {'posts': posts})
     else:
